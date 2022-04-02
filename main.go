e56182eac2963a9bfd5773f8300de0142c50b65b
Juwon-Yun <eooll2@daum.net>
2022-04-02 23:55:39 +0900

    feat: gitignore

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
 func main() {
+	_, err := gorm.Open(mysql.Open(), &gorm.Config{})
+
+	if err != nil {
+		panic("could not connect to the dataBase")
+	}
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
